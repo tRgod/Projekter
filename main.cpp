199598b5b6c06d000ea68fe094ae0a4baeacce65
thor <thor5970@hotmail.com>
2017-08-23 15:25:57 +0200

    Start på bib ting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
 using namespace std;
 int main(){
+    using namespace std::this_thread;
+    using namespace std:: chrono;
     cout<<"Hello World"<< endl;
-    wait(1000);
-    cout<<"what is youre name ?"<< endl;
+    sleep_for(seconds(3));
+
+    cout<<"What is youre name ?"<< endl;
+    string mystring;
+    cin>>mystring;
+    cout<<"Wlcome to the libary "<<mystring<< endl;
+    enum commands_t {Time, Books, Video, Storage};
+    commands_t command; 
+    cout<<"The Following Commands are avalibale\nTIme\nBooks\nVideo\nStorage"<<endl;
+    cin>>command; 
+    
 }
